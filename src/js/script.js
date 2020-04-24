ce36e416df286db325bbebad7ac01b9085edb3e6
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-04-24 11:25:00 +0200

    Add new method getElements in AmountWidget class

--- src/js/script.js
+++ src/js/script.js
@@ -215,6 +215,15 @@
       console.log('constructor arguments:', element);
     }
 
+    getElements(element){
+      const thisWidget = this;
+
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+
   }
 
   const app = {
