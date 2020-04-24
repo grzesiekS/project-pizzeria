227ac17fa6e25bed0a058e432bffb338a25a10ef
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-04-24 12:17:54 +0200

    Add method initActions in AmountWidget class

--- src/js/script.js
+++ src/js/script.js
@@ -212,6 +212,8 @@
       const thisWidget = this;
 
       thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.initActions();
 
       console.log('AmountWidget:', thisWidget);
       console.log('constructor arguments:', element);
@@ -226,6 +228,25 @@
 
       thisWidget.value = newValue;
       thisWidget.input.value = thisWidget.value;
+    }
+
+    initActions() {
+      const thisWidget = this;
+
+      thisWidget.input.addEventListener('change', function(){
+        thisWidget.setValue(thisWidget.input.value);
+      });
+
+      thisWidget.linkDecrease.addEventListener('click', function(){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value - 1);
+      });
+
+      thisWidget.linkIncrease.addEventListener('click', function(){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value + 1);
+      });
+
     }
 
     getElements(element){
