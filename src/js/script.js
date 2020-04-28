b9333b220d3bbae7673b71c082a18ead2938ce15
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-04-28 17:31:42 +0200

    Add function initActions in class Cart

--- src/js/script.js
+++ src/js/script.js
@@ -353,8 +353,17 @@
 
       thisCart.products = [];
       thisCart.getElements(element);
+      thisCart.initActions();
 
       console.log('new Cart', thisCart);
+    }
+
+    initActions() {
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
     }
 
     getElements(element) {
