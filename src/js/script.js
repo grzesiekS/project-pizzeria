65ae4ac937ba0024f686b633f3e984315ed82b90
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-05-02 11:33:56 +0200

    Init a connection to a json server in app object

--- src/js/script.js
+++ src/js/script.js
@@ -593,14 +593,30 @@
       //console.log('thisApp.data:', thisApp.data);
 
       for(let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function() {
       const thisApp = this;
+      const url = settings.db.url + '/' + settings.db.product;
 
-      thisApp.data = dataSource;
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+
+          /* [DONE] save parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+          /* [DONE] execute initMenu method */
+          thisApp.initMenu();
+        });
+
+      console.log('thisApp.data', JSON.stringify(thisApp.data));
+
+      thisApp.data = {};
     },
 
     init: function(){
@@ -612,7 +628,6 @@
       //console.log('templates:', templates);
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
 
