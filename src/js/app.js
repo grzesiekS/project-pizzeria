135ded4858709856d2150b921731f3e57de6b7c3
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-05-05 18:25:16 +0200

    Add event listener for new custom event

--- src/js/app.js
+++ src/js/app.js
@@ -51,6 +51,12 @@ const app = {
 
     const cartElem = document.querySelector(select.containerOf.cart);
     thisApp.cart = new Cart(cartElem);
+
+    thisApp.productList = document.querySelector(select.containerOf.menu);
+
+    thisApp.productList.addEventListener('add-to-cart', function(event){
+      app.cart.add(event.detail.product);
+    });
   },
 };
 
