5ea46d56b991b999114d086238953d271642711c
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-05-13 19:02:59 +0200

    Add method verifyTableSelection

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -212,54 +212,69 @@ export class Booking {
 
   }
 
+  verifyTableSelection() {
+    const thisBooking = this;
+
+    const bookedTable = thisBooking.dom.bookingWrapper
+      .querySelector(select.booking.tables + '.' + classNames.booking.tableSelected);
+
+    return bookedTable == null ? true : false;
+  }
+
   bookTable() {
     const thisBooking = this;
     const url = settings.db.url + '/' + settings.db.booking;
+    const tableVerifyResult = thisBooking.verifyTableSelection();
 
-    const duration = parseInt(thisBooking.dom.hoursAmount.querySelector('input').value);
-    const pplAmount = parseInt(thisBooking.dom.peopleAmount.querySelector('input').value);
-    const tableSelected = parseInt(thisBooking.dom.bookingWrapper
-      .querySelector(select.booking.tables + '.' + classNames.booking.tableSelected)
-      .getAttribute('data-table'));
+    /* IF: verify if table was selected */
+    if(!tableVerifyResult) {
+      const duration = parseInt(thisBooking.dom.hoursAmount.querySelector('input').value);
+      const pplAmount = parseInt(thisBooking.dom.peopleAmount.querySelector('input').value);
+      const tableSelected = parseInt(thisBooking.dom.bookingWrapper
+        .querySelector(select.booking.tables + '.' + classNames.booking.tableSelected)
+        .getAttribute('data-table'));
 
-    const payload = {
-      date: thisBooking.datePicker.value,
-      hour: thisBooking.hourPicker.value,
-      table: tableSelected,
-      duration: duration,
-      ppl: pplAmount,
-      starters: [],
-    };
+      const payload = {
+        date: thisBooking.datePicker.value,
+        hour: thisBooking.hourPicker.value,
+        table: tableSelected,
+        duration: duration,
+        ppl: pplAmount,
+        starters: [],
+      };
 
-    /*START LOOP: For all inputs in checkbox for starters */
-    for(let input of thisBooking.dom.starters){
-      /*IF: input is checked */
-      if(input.checked){
-        /* [DONE] Add input value to payload.starters */
-        payload.starters.push(input.value);
-      /*END IF*/
+      /*START LOOP: For all inputs in checkbox for starters */
+      for(let input of thisBooking.dom.starters){
+        /*IF: input is checked */
+        if(input.checked){
+          /* [DONE] Add input value to payload.starters */
+          payload.starters.push(input.value);
+        /*END IF*/
+        }
+        /*END LOOP: For all inputs in checkbox for starters */
       }
-      /*END LOOP: For all inputs in checkbox for starters */
+
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload),
+      };
+
+      fetch(url, options)
+        .then(function(response){
+          return response.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+          thisBooking.getData();
+        });
+
+      thisBooking.removeActiveTables();
+    /* END IF */
     }
 
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(payload),
-    };
-
-    fetch(url, options)
-      .then(function(response){
-        return response.json();
-      })
-      .then(function(parsedResponse){
-        console.log('parsedResponse', parsedResponse);
-        thisBooking.getData();
-      });
-
-    thisBooking.removeActiveTables();
   }
 
   render(element) {
