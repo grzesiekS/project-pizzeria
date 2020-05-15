789c8cc6c1fa03538f2d6223b294a108b1ffe482
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-05-15 16:40:36 +0200

    Add changing class active methods

--- src/js/components/MainPage.js
+++ src/js/components/MainPage.js
@@ -9,12 +9,12 @@ class MainPage {
   }
 
 
-  /*TODO: Function that iterate through numbers of courusels items */
+  /*[DONE] Function that iterate through numbers of courusels items */
   idOfCarouselElem(){
     const thisMainPage = this;
 
     /*[DONE] get id of active item in carousel and put it in variable for selecting next element*/
-    let n = Array.from(thisMainPage.dom.carouselItems).indexOf(thisMainPage.dom.activeCarouselItem);
+    let n = Array.from(thisMainPage.dom.carouselItems).indexOf(thisMainPage.dom.wrapper.querySelector(select.mainPage.carousel.activeCarouselItem));
 
     /*START IF: Check if Id of active item is the last in array */
     if(n >= thisMainPage.dom.carouselItems.length - 1){
@@ -28,11 +28,34 @@ class MainPage {
     return n;
   }
 
+  removeActiveClassCarouselItem(){
+    const thisMainPage = this;
+
+    /*START LOOP: for all carousel items */
+    for(let item of thisMainPage.dom.carouselItems){
+      /* [DONE] remove active class */
+      item.classList.remove('active');
+
+    /*END LOOP: for all carousel items */
+    }
+  }
+
+  changeActiveCarouselItem(n){
+    const thisMainPage = this;
+
+    /* [DONE] remove active class */
+    thisMainPage.removeActiveClassCarouselItem();
+    /* [DONE] change active carousel item */
+    thisMainPage.dom.carouselItems[n].classList.add('active');
+  }
+
   initAction(){
     const thisMainPage = this;
 
     window.addEventListener('load', function(){
-      this.setInterval(function() {thisMainPage.idOfCarouselElem();}, 4000);
+      this.setInterval(function()
+      {thisMainPage.changeActiveCarouselItem(thisMainPage.idOfCarouselElem());}
+      , 8000);
     });
   }
 
@@ -44,7 +67,7 @@ class MainPage {
 
     thisMainPage.dom.carouselWrapper = thisMainPage.dom.wrapper.querySelector(select.mainPage.carousel.carouselWrapper);
     thisMainPage.dom.carouselItems = thisMainPage.dom.wrapper.querySelectorAll(select.mainPage.carousel.carouselItem);
-    thisMainPage.dom.activeCarouselItem = thisMainPage.dom.wrapper.querySelector(select.mainPage.carousel.activeCarouselItem);
+
   }
 }
 
