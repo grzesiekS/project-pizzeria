554b345ee04fa76de48d154a8df89fab5980511d
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-05-15 15:30:28 +0200

    Add getElement method in MainPage class

--- src/js/components/MainPage.js
+++ src/js/components/MainPage.js
@@ -1,8 +1,21 @@
+import { select } from '../settings.js';
+
 class MainPage {
-  constructor() {
+  constructor(element) {
     const thisMainPage = this;
 
+    thisMainPage.getElement(element);
     console.log(thisMainPage);
+  }
+
+  getElement(element) {
+    const thisMainPage = this;
+
+    thisMainPage.dom = {};
+    thisMainPage.dom.wrapper = element;
+
+    thisMainPage.dom.carouselWrapper = thisMainPage.dom.wrapper.querySelector(select.mainPage.carousel.carouselWrapper);
+    thisMainPage.dom.carouselItem = thisMainPage.dom.wrapper.querySelectorAll(select.mainPage.carousel.carouselItem);
   }
 }
 
